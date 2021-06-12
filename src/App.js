1234fec65a880d3550f602ee04f6a0cc3ffb08af
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-06-12 16:26:56 +0300

    açıklama eklendi

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Dashboard from './layouts/Dashboard';
 import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react'
 import Navi from './layouts/Navi';
+
+// npx create-react-app hrms-frontend // React projesi oluşturur
 function App() {
   return (
     <div className="App">
